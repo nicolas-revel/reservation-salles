322979820316b802f3c8f3f6a0dc76e0ff23694e
Nicolas Revel <nicolas.revel@lapalteforme.io>
2020-12-16 15:35:25 +0100

    Mise en place de la page de profil (update, disconnect, delete)

--- classes/user.php
+++ classes/user.php
@@ -30,16 +30,29 @@ class user
     return $this->password;
   }
 
-  public function register($login, $password)
+  private function verifUser($login)
   {
     $pdo = new PDO("mysql:host=localhost;dbname=test", "root", "");
     $requete = "SELECT * FROM utilisateurs WHERE login = :login";
     $query = $pdo->prepare($requete);
     $query->execute([
-      ":login" => $this->login
+      ":login" => $login
     ]);
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
-    if (empty($result)) {
+    if (!empty($result)) {
+      return $result;
+    } else {
+      return false;
+    }
+  }
+
+  public function register($login, $password)
+  {
+    $login = htmlspecialchars(trim($login));
+    $password = htmlspecialchars(trim(password_verify($password, PASSWORD_BCRYPT)));
+    $checkuser = $this->verifUser($login);
+    if ($checkuser === false) {
+      $pdo = new PDO("mysql:host=localhost;dbname=test", "root", "");
       $requete = "INSERT INTO utilisateurs (login, password) VALUES (:login, :password)";
       $query = $pdo->prepare($requete);
       $result = $query->execute([
@@ -58,7 +71,7 @@ class user
     $requete = "SELECT * FROM utilisateurs WHERE login = :login";
     $query = $pdo->prepare($requete);
     $query->execute([
-      ":login" => $this->login
+      ":login" => $login
     ]);
     $result = $query->fetch(PDO::FETCH_ASSOC);
     if (!empty($result) && password_verify($password, $result['password'])) {
@@ -91,23 +104,24 @@ class user
 
   public function update($login, $password)
   {
-    $pdo = new PDO("mysql:host=localhost;dbname=classes", "root", "");
-    if (!empty($login)) {
-      $this->login = htmlspecialchars(trim($login));
+    $login = htmlspecialchars(trim($login));
+    $password = htmlspecialchars(trim(password_verify($password, PASSWORD_BCRYPT)));
+    $checkuser = $this->verifUser($login);
+    if ($checkuser === false) {
+      $pdo = new PDO("mysql:host=localhost;dbname=test", "root", "");
+      if (!empty($login)) {
+        $this->login = htmlspecialchars(trim($login));
+      }
+      if (!empty($password)) {
+        $this->password = htmlspecialchars(trim(password_hash($password, PASSWORD_BCRYPT)));
+      }
+      $requete = "UPDATE `utilisateurs` SET login = :login , password = :password WHERE id = :id";
+      $query = $pdo->prepare($requete);
+      $query->execute([
+        ':id' => $this->id,
+        ':login' => $this->login,
+        ':password' => $this->password
+      ]);
     }
-    if (!empty($password)) {
-      $this->password = htmlspecialchars(trim(password_hash($password, PASSWORD_BCRYPT)));
-    }
-    $requete = "UPDATE `utilisateurs` SET login = :login , password = :password WHERE id = :id";
-    $query = $pdo->prepare($requete);
-    $query->execute([
-      ':id' => $this->id,
-      ':login' => $this->login,
-      ':password' => $this->password
-    ]);
-  }
-
-  public function refresh()
-  {
   }
 }
